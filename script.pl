90ae8bb79bb0d2f9ce6aa46a0ce9d6ff3aae1f27
Maxim Sviridenko <maxim@emrk.ru>
2016-01-09 23:48:14 +0700

    script init

---
+++ script.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: script.pl
+#
+#        USAGE: ./script.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 09.01.2016 23:43:44
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+
