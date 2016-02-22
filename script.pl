eaf44465a51f0a2e2e7380a46c813d5a7fab938b
Maxim Sviridenko <maxim@emrk.ru>
2016-02-23 00:13:32 +0700

    edit script for reading list files

--- script.pl
+++ script.pl
@@ -8,14 +8,14 @@ use DBIx::Custom;
 use YAML::XS 'LoadFile';
 
 my $config = LoadFile('config.yaml');
+my @log = ();#Logfile data
 
 chdir('logs');
 my @src_files = <*.dat>;
 my @src_data = ();
 
-print @src_files;
 my $src_file;
-exit;
+
 my $dbi = DBIx::Custom->connect(
    dsn => "dbi:mysql:database=$config->{database}",
    user => $config->{user},
@@ -23,26 +23,47 @@ my $dbi = DBIx::Custom->connect(
    option => {mysql_enable_utf8 => 1}
 );
 
-open (SOURCE,"< $src_file") || die "Can't open file: $src_file";
-@src_data = split(' ',substr(<SOURCE>,,23));
-close SOURCE;
+my $file = 0;
+while(<@src_files>){
+	$file++;#Files counting
+	push @log,"Parsing file: $_";
 
-print localtime(time)."\n";
-foreach my $key(@src_data){
-	my($name,$value) = split('=',$key);
-	$value=~s/(,)/./; #Change separate
+	open (SOURCE,"< $_") || die "Can't open file: $src_file";
+	@src_data = <SOURCE>;
+	close SOURCE;
 	
-	print "SENSOR:$name\tVALUE:$value\n";
-	#push @data, {name => $name, value => $value}; Feature for multiple insert ORM
+	push @log,"Sensor readings: ".@src_data;
+	my $line = 0;
+	print "\e[H\e[J";#Clear display
 
-	$dbi->insert(
-		{
-			name => $name,
-			value => "$value",
-		},
-		ctime => 'ctime',
-		table => 'temp',
-	);
+	foreach my $key(@src_data){
+		$line++;
+		print "\e[K\e[H>Reading file $file:".@src_files."\t$line:".@src_data." lines\n";
+
+		my ($date,$time) = split(' ',substr($key,1,19));
+		my @sensors = split(' ',substr($key,23));
+		
+		foreach my $sensor(@sensors){
+			my($name,$value) = split('=',$sensor);
+			$value=~s/(,)/./; #Change separate
+       		$dbi->insert(
+	        	{
+    	    	    datadate => $date,
+        	    	datatime => $time,
+	        	    name => $name,
+    	        	value => "$value",
+	        	},
+		        ctime => 'ctime',
+    	    	table => 'temp',
+	    	);
+		};
+	};
 };
+
+open (LOGFILE,"> $config->{logfile}") || die "Can't open logfile: $config->{logfile}";
+	foreach my $log(@log){
+		print LOGFILE "$log\n";
+	};
+close LOGFILE;
 
 1;
